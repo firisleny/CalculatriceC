824ce072787bfa6cb5e7f028d5d4f2290a528ef5
firisleny <mathReseau@proton.me>
2024-05-10 16:08:50 +0200

    Ajout des fonction pour deux chiffre

--- calcul.c
+++ calcul.c
@@ -2,17 +2,57 @@
 #include "calcul.h"
 
 void addition() {
-	printf("Vous avez choisie le mods addition\n");
+	printf("Vous avez choisie le mods addition\n\n");
+
+	int chiffreUn;
+	int chiffreDeux;
+
+	printf("Veuillez entre votre premier chiffre : ");
+	scanf("%d", &chiffreUn);
+	printf("Veuillez entre votre deuxieme chiffre : ");
+	scanf("%d", &chiffreDeux);
+
+	printf("\nLe resultat de %d + %d est de %d", chiffreUn, chiffreDeux, ( chiffreUn + chiffreDeux ));
 }
 
 void soustraction() {
 	printf("Vous avez choisie le mods soustraction\n");
+
+	int chiffreUn;
+	int chiffreDeux;
+
+	printf("Veuillez entre votre premier chiffre : ");
+	scanf("%d", &chiffreUn);
+	printf("Veuillez entre votre deuxieme chiffre : ");
+	scanf("%d", &chiffreDeux);
+
+	printf("\nLe resultat de %d - %d est de %d", chiffreUn, chiffreDeux, (chiffreUn - chiffreDeux));
 }
 
 void multiplication() {
 	printf("Vous avez choisie le mods multiplication\n");
+
+	int chiffreUn;
+	int chiffreDeux;
+
+	printf("Veuillez entre votre premier chiffre : ");
+	scanf("%d", &chiffreUn);
+	printf("Veuillez entre votre deuxieme chiffre : ");
+	scanf("%d", &chiffreDeux);
+
+	printf("\nLe resultat de %d * %d est de %d", chiffreUn, chiffreDeux, (chiffreUn * chiffreDeux));
 }
 
 void division() {
 	printf("Vous avez choisie le mods division\n");
+
+	int chiffreUn;
+	int chiffreDeux;
+
+	printf("Veuillez entre votre premier chiffre : ");
+	scanf("%d", &chiffreUn);
+	printf("Veuillez entre votre deuxieme chiffre : ");
+	scanf("%d", &chiffreDeux);
+
+	printf("\nLe resultat de %d / %d est de %d", chiffreUn, chiffreDeux, (chiffreUn / chiffreDeux));
 }
